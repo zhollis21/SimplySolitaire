259b528f63626457328dfb06e24bb8d6109a794f
Paula Oliveros Hollis <paulaoliverosc@gmail.com>
2019-09-07 10:38:41 -0400

    Created the card object.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public enum Suit { Clubs, Diamonds, Hearts, Spades};
+    public enum Value { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace};
+
+    public Suit CardSuit;
+    public Value CardValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
