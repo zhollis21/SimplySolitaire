1dc067781df1331f3fb7edfc54329bc3fcb2d1e2
gex713 <gex713@yahoo.com>
2019-09-10 19:11:16 -0400

    Added a card stack with  a push and pop method.

---
+++ Assets/Scripts/CardStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class CardStack
+    {
+        public List<Card> Cards = new List<Card>();
+
+        public void Push(Card newCard)
+        {
+            Cards.Add(newCard);
+        }
+
+        public Card Pop()
+        {
+            Card lastCard = Cards.Last();
+            Cards.Remove(lastCard);
+
+            return lastCard;
+        }
+    }
+}
