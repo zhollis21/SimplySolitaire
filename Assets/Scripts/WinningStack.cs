73a6a57236fd060d059814c8a9c28d1fca3dbb91
gex713 <gex713@yahoo.com>
2019-09-17 18:56:37 -0400

    Adding the beginning of the winning stack

---
+++ Assets/Scripts/WinningStack.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WinningStack : CardStack
+{
+    public bool IsCompleted()
+    {
+        return false;
+    }
+
+    public bool CanDrop(Card cardToDrop)
+    {
+        if (cards.Count < 1 && cardToDrop.CardValue == Card.Value.Ace)
+        {
+            return true;
+        }
+        var blah = cards.Last();
+        if (blah.CardSuit != cardToDrop.CardSuit)
+        {
+            return false;
+        }
+        //ToDo do value comparison
+        return true;
+    }
+
+    public void OnDrop(Card cardToDrop)
+    {
+        if (CanDrop(cardToDrop))
+        {
+            //do stuff
+
+        }
+    }
+}
